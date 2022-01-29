a800e84a77f929af4738e95d3786f60757ba6160
Jerry <C110181116@nkust.edu.tw>
2022-01-29 21:16:20 +0800

    01/29

--- JavaClass_0125/Web/HomeWork/hex.js
+++ JavaClass_0125/Web/HomeWork/hex.js
@@ -1,13 +1,9 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 function changeColor(){
-    var code = [];
-    for (var i = 0 ; i <= 5 ; i++){
-        var codeOrder = Math.floor(Math.random() * hex.length);
-        code[i] = hex[codeOrder]; 
-    }
-    var colorCode = '#';
-    for (var i = 0 ; i < code.length ; i++){
-        colorCode += code[i];
+    let colorCode = '#';
+    for (let i = 0 ; i <= 5 ; i++){
+        let codeOrder = Math.floor(Math.random() * hex.length);
+        colorCode += hex[codeOrder]; 
     }
     document.body.style.backgroundColor = colorCode;
     document.getElementById("color").innerHTML = colorCode;
